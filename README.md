404a0ea26081033b4674e567ee17d969b0ab7897
BrOrlandi <brorlandi@gmail.com>
2014-11-11 10:40:00 -0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+Live Search Ajax
+==========
+
+A simple example to show how to implement the [Live Search Ajax Pattern](http://ajaxpatterns.org/Live_Search).
+This sample uses [jQuery](http://jquery.com/) to make the Ajax request the server was implemented with [Flask Python](flask.pocoo.org).
+
+##Installing
+
+`pip install flask`
+
+## Running
+
+`python app.py` 
+
+*Developed by Bruno Orlandi*
---
+++ app.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+from flask import Flask, request, render_template
+import json
+app = Flask(__name__)
+app.config.update(dict(
+    DEBUG=True,
+))
+
+BRAZIL_STATES = [u"Acre - Rio Branco",
+u"Alagoas - Maceió",
+u"Amapá - Macapá",
+u"Amazonas - Manaus",
+u"Bahia - Salvador",
+u"Ceara - Fortaleza",
+u"Distrito Federal - Brasília",
+u"Espírito Santo - Vitória",
+u"Goiás - Goiânia",
+u"Maranhão - São Luiz",
+u"Mato Grosso - Cuiabá",
+u"Mato Grosso do Sul - Campo Grande",
+u"Minas Gerais - Belo Horizonte",
+u"Pará - Belém",
+u"Paraíba - João Pessoa",
+u"Paraná - Curitiba",
+u"Pernambuco - Recife",
+u"Piauí - Terezina",
+u"Rio de Janeiro - Rio de Janeiro",
+u"Rio Grande do Norte - Natal",
+u"Rio Grande do Sul - Porto Alegre",
+u"Rondônia - Porto Velho",
+u"Roraima - Boa Vista",
+u"Santa Catarina - Florianópolis",
+u"São Paulo - São Paulo",
+u"Sergipe - Aracajú",
+u"Tocantins - Palmas"]
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/search")
+def search():
+	text = request.args['searchText']
+	result = [c for c in BRAZIL_STATES if unicode(text).lower() in c.lower()]
+	return json.dumps({"results":result})
+
+if __name__ == "__main__":
+    app.run()
